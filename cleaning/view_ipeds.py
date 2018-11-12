0885debaa3e4a7ee56fe248a0c466cec630e5792
nvonturk <nvonturk@gmail.com>
2018-11-11 20:14:22 -0500

    Completd cleaning of key financial indicators and initial visualizations - seems like a drop in total core rev and exp in 2015 -> TODO, understand if there was a methodological shift that led to that drop like a drop in reporting institutions

--- cleaning/view_ipeds.py
+++ cleaning/view_ipeds.py
@@ -1,12 +1,30 @@
 import pandas as pd 
 import numpy as np 
 import os
+import matplotlib.pyplot as plt
 
 # Name directory or directories of source files
 
 financial_cleaned = "../raw/financial/raw_financial_cleaned.csv"
 df = pd.read_csv(financial_cleaned)
+df = df.set_index(['unitid', 'year', 'institution name'])
+df.sort_values(by=['year'], inplace=True)
 
+revenues = [r for r in df.columns.values if ("revenue" in r or "Revenue" in r)]
+expenses = [e for e in df.columns.values if ("expense" in e or "Expense" in e)]
 
+print(revenues)
+print(expenses)
 
+all_revenues = df.xs('Revenues from tuition and fees per FTE', axis=1)
+all_expenses = df.xs('Instruction expenses per FTE', axis=1)
+
+total_rev = all_revenues.groupby(level=1).sum()
+total_exp = all_expenses.groupby(level=1).sum()
+years = [2010 + i for i in range(8)]
+
+print(total_rev.index)
+
+plt.plot(total_rev.index, total_rev, 'g', total_exp.index, total_exp, 'r')
+plt.show()
 
